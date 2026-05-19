7b04ae116ae8b11f583f3f6011165b80aeefdd90
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-05-19 14:37:56 +0800

    Export gridded wind transform

--- Scripts/Homework/4/transform/__init__.py
+++ Scripts/Homework/4/transform/__init__.py
@@ -1,5 +1,6 @@
-from .transform import merge_forecast_hours, run_transform, transform_station_wind
+from .transform import merge_forecast_hours, run_transform, transform_gridded_wind, transform_station_wind
 from .wind import (
+    build_gridded_wind_dataset,
     build_station_wind_dataframe,
     build_station_wind_ml_dataframe,
     extract_wind,
@@ -7,11 +8,13 @@ from .wind import (
 )
 
 __all__ = [
+    "build_gridded_wind_dataset",
     "build_station_wind_dataframe",
     "build_station_wind_ml_dataframe",
     "extract_wind",
     "extract_wind_features",
     "merge_forecast_hours",
     "run_transform",
+    "transform_gridded_wind",
     "transform_station_wind",
 ]
