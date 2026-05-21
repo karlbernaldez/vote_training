b92593f549e50dfecc57b0989a11227fd952d954
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-05-21 10:05:13 +0800

    feat(gfs-ml): implement CNN training loop

--- pipelines/atmospheric/gfs/ml/training/train_cnn.py
+++ pipelines/atmospheric/gfs/ml/training/train_cnn.py
@@ -1,2 +1,23 @@
-def train_cnn():
-    print('GFS CNN training scaffold active')
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from pipelines.atmospheric.gfs.ml.models.cnn import GFSForecastCNN
+
+
+def train_cnn(features, targets, epochs=10, batch_size=32, lr=1e-3):
+    dataset = TensorDataset(features, targets)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    model = GFSForecastCNN()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.MSELoss()
+
+    model.train()
+    for epoch in range(epochs):
+        for xb, yb in loader:
+            optimizer.zero_grad()
+            preds = model(xb)
+            loss = loss_fn(preds, yb)
+            loss.backward()
+            optimizer.step()
+
+    return model
