c2f85ec6932d47e3675f0ffacf4189699fb25763
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-05-21 09:15:04 +0800

    feat: wire WW3 pipeline to transforms

--- pipelines/ocean/ww3/pipeline.py
+++ pipelines/ocean/ww3/pipeline.py
@@ -1,5 +1,7 @@
 from pipelines.ocean.ww3.ingest.ingest_ww3 import main
+from pipelines.ocean.ww3.transform.merge_forecast import merge_wave_transforms
 
 def run_pipeline():
     main()
-    print('WW3 pipeline scaffold active')
+    merge_wave_transforms()
+    print('WW3 pipeline active')
