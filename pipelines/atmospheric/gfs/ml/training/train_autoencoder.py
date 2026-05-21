fa3cc55a8bcfdfe9304f0dba806401f0720a9e72
Karl Santiago Baydal Bernaldez <bernaldezkarlsantiago@gmail.com>
2026-05-21 10:21:45 +0800

    feat(gfs-ml): implement autoencoder training

--- pipelines/atmospheric/gfs/ml/training/train_autoencoder.py
+++ pipelines/atmospheric/gfs/ml/training/train_autoencoder.py
@@ -1,2 +1,23 @@
-def train_autoencoder():
-    print('GFS autoencoder training scaffold active')
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+from pipelines.atmospheric.gfs.ml.models.autoencoder import GFSForecastAutoencoder
+
+
+def train_autoencoder(features, epochs=10, batch_size=32, lr=1e-3):
+    dataset = TensorDataset(features, features)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    model = GFSForecastAutoencoder()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+    loss_fn = torch.nn.MSELoss()
+
+    model.train()
+    for _ in range(epochs):
+        for xb, yb in loader:
+            optimizer.zero_grad()
+            preds = model(xb)
+            loss = loss_fn(preds, yb)
+            loss.backward()
+            optimizer.step()
+
+    return model
